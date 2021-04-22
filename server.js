719a3331cc62e9cd1bd218f33dafd976e79414a6
BuildTools <unconfigured@null.spigotmc.org>
2021-04-21 20:20:45 -0400

    Add Files

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-/* const express = require("express");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const port = 3000; */
 const io = require('socket.io')(3001, {
   cors: {
       origin: 'http://localhost:3000',
